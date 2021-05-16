90c671946bfd990a3afb4bcf36167bcf88d00671
AlLevykin <a.a.levykin@gmail.com>
2021-05-16 14:53:50 +0500

    Console app created

---
+++ Byndyusoft.ConsoleCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Byndyusoft.ConsoleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello ConsoleCalculator!");
+        }
+    }
+}
